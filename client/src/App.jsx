24bdfa679ed9710c9e855a0662f830244c744122
dianalynshull <dianalynshull@gmail.com>
2021-02-13 18:22:48 -0700

    added the homepage bio of Figure and styled it. Also forced the body to be the view height of the device

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Components/Header/Header';
+import Homepage from './Components/Homepage/Homepage';
 import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Homepage />
       <Footer />
     </>
   );
